f5f4e168bd3d298eeac9401d155874f503ba862c
Irvan Fauziansyah <ervhan@gmail.com>
2023-07-06 01:56:05 +0700

    Add simple authentication method in the API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  # Simple authentication by checking if the request has the correct api key
+  # In the real world application, we should use a more secure authentication method
+  def authenticate
+    api_key = request.headers['Api-Key']
+
+    return if api_key == Rails.application.credentials.api_key
+
+    render json: { status: 'error', message: 'Request is unauthorized' }, status: :unauthorized
+  end
 end
--- test/controllers/api/v1/bookings_controller_test.rb
+++ test/controllers/api/v1/bookings_controller_test.rb
@@ -6,22 +6,30 @@ module Api
       def setup
         @payload_two = JSON.parse(file_data('payload_two.json'))
         @payload_one = JSON.parse(file_data('payload_one.json'))
+        @api_key = Rails.application.credentials.api_key
+      end
+
+      test 'it should return error when api key is invalid' do
+        post api_v1_bookings_url, params: @payload_one, as: :json
+
+        assert_response :unauthorized
+        assert_equal({ 'status' => 'error', 'message' => 'Request is unauthorized' }, response.parsed_body)
       end
 
       test 'it should successfully create booking' do
-        post api_v1_bookings_url, params: @payload_one, as: :json
+        post api_v1_bookings_url, headers: { 'Api-Key' => @api_key }, params: @payload_one, as: :json
 
         assert_response :created
       end
 
       test 'it should successfully create booking with different payload' do
-        post api_v1_bookings_url, params: @payload_two, as: :json
+        post api_v1_bookings_url, headers: { 'Api-Key' => @api_key }, params: @payload_two, as: :json
 
         assert_response :created
       end
 
       test 'it should return error when payload is empty' do
-        post api_v1_bookings_url, as: :json, params: nil
+        post api_v1_bookings_url, headers: { 'Api-Key' => @api_key }, as: :json, params: nil
 
         assert_response :bad_request
         assert_equal({ 'status' => 'error', 'message' => 'Payload cannot be empty' }, response.parsed_body)
@@ -30,7 +38,7 @@ module Api
       test 'it should return error when payload is invalid' do
         @payload_one['guest']['email'] = nil
 
-        post api_v1_bookings_url, params: @payload_one, as: :json
+        post api_v1_bookings_url, headers: { 'Api-Key' => @api_key }, params: @payload_one, as: :json
 
         assert_response :unprocessable_entity
         assert_equal({ 'status' => 'error', 'message' => 'Email can\'t be blank' }, response.parsed_body)
