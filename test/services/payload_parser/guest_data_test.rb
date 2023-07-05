b464d4b214cd620b07b09a5423dd8323055af204
Irvan Fauziansyah <ervhan@gmail.com>
2023-07-05 16:43:10 +0700

    Fix test payload

--- test/services/payload_parser/guest_data_test.rb
+++ test/services/payload_parser/guest_data_test.rb
@@ -3,13 +3,57 @@ require "test_helper"
 module PayloadParser
   class GuestDataTest < ActiveSupport::TestCase
     def setup
-      @payload_one = payload_one
-      @payload_two = payload_two
+      @payload_one = {
+        "reservation_code": "YYY12345678",
+        "start_date": "2021-04-14",
+        "end_date": "2021-04-18",
+        "nights": 4,
+        "guests": 4,
+        "adults": 2,
+        "children": 2,
+        "infants": 0,
+        "status": "accepted",
+        "guest": {
+          "first_name": "Wayne",
+          "last_name": "Woodbridge",
+          "phone": "639123456789",
+          "email": "wayne_woodbridge@bnb.com"
+        },
+        "currency": "AUD",
+        "payout_price": "4200.00",
+        "security_price": "500",
+        "total_price": "4700.00"
+      }
+      @payload_two = {
+        "reservation": {
+          "code": "XXX12345678",
+          "start_date": "2021-03-12",
+          "end_date": "2021-03-16",
+          "expected_payout_amount": "3800.00",
+          "guest_details": {
+            "localized_description": "4 guests",
+            "number_of_adults": 2,
+            "number_of_children": 2,
+            "number_of_infants": 0
+          },
+          "guest_email": "wayne_woodbridge@bnb.com",
+          "guest_first_name": "Wayne",
+          "guest_last_name": "Woodbridge",
+          "guest_phone_numbers": %w[639123456789 639123456789],
+          "listing_security_price_accurate": "500.00",
+          "host_currency": "AUD",
+          "nights": 4,
+          "number_of_guests": 4,
+          "status_type": "accepted",
+          "total_paid_amount_accurate": "4300.00"
+        }
+      }
     end
 
     test 'runs successfully for payload one' do
       data = PayloadParser::GuestData.run(payload: @payload_one)
 
+      assert data.valid?
       assert_equal Hash, data.result.class
       assert data.result.keys.any? { |key| key.match(/email/) }, "No Key contains 'email'"
     end
@@ -17,6 +61,7 @@ module PayloadParser
     test 'runs successfully for payload two' do
       data = PayloadParser::GuestData.run(payload: @payload_two)
 
+      assert data.valid?
       assert_equal Hash, data.result.class
       assert data.result.keys.any? { |key| key.match(/email/) }, "No Key contains 'email'"
     end
